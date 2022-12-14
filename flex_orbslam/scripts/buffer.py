87bdb4d83c45b6a53be97947c091d7a09fc08764
Vivek_W <vivekw964@gmail.com>
2022-12-13 19:04:03 -0500

    Uploaded  ROS python package
    flex_orbslam package containing python scripts to implement flexibility utilizing ORB_SLAM3

---
+++ flex_orbslam/scripts/buffer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+import rospy
+import rosbag
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from std_msgs.msg import Bool
+from sensor_msgs.msg import Image
+
+vel_pub = rospy.Publisher("robot_velocity", Int32, queue_size=1)
+img_pub = rospy.Publisher('/camera/image_raw', Image, queue_size = 1)
+
+
+def imagecallback(img):
+    # print("i am inside img callback\n")
+    global img_msg, img_pub
+    img_msg = img
+    rate = rospy.Rate(2)
+    img_pub.publish(img_msg)
+    print("image published")
+    rospy.sleep(0.1)
+
+
+def tlosscallback(flag):
+    global t_flag, vel_pub
+    t_flag = flag.data
+    rate = rospy.Rate(2)
+    print("flag value in buffer:", t_flag)
+
+    if t_flag == False:
+        velocity = 3
+        vel_pub.publish(velocity)
+        rospy.sleep(0.1)
+    elif t_flag == True:
+        velocity = 0
+        vel_pub.publish(velocity)
+        rospy.sleep(0.1)
+
+
+def main_func():
+    print("Starting...")
+    global vel_pub
+    rospy.init_node('buffer', anonymous=True)
+    rospy.Subscriber("/camera/images", Image, imagecallback)
+    rospy.Subscriber("/track_loss", Bool, tlosscallback)
+    # rospy.sleep(0.1)
+    velo = 3
+    count = 0
+    while not rospy.is_shutdown():
+        # print("reached in while")
+        if count <= 3:
+            # print(count)
+            vel_pub.publish(velo)
+            count += 1
+            rospy.sleep(0.05)
+
+
+if __name__ == '__main__':
+    try:
+        main_func()
+
+    except rospy.ROSInterruptException:
+        pass
+
---
+++ flex_orbslam/scripts/emulator.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+import rospy
+import rosbag
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+from sensor_msgs.msg import Image
+
+current_frame = 0
+published_messages = 0
+count = 0
+
+image_list = []
+bag = rosbag.Bag('/home/vivekw964/vivek_ws/src/flex_orbslam/scripts/MH_01_easy.bag')
+for topic, msg, t in bag.read_messages(topics =['/cam0/image_raw', '/imu', '/position']):
+    image_list.append(msg)
+
+def velcallback(data_vel):
+
+    print ('Received velocity', data_vel.data)
+    global processing, new_velocity, velocity, count
+    pub = rospy.Publisher('/camera/images', Image, queue_size = 1)
+    global image_list, current_frame, published_messages
+    rate = rospy.Rate(2)
+    # count = 0
+    next_frame = 0
+
+    velocity = data_vel.data
+ 
+    while current_frame < len(image_list):
+        # print("under while loop")
+        pub.publish(image_list[current_frame])
+        print("Frame being published: ",current_frame)
+        published_messages += 1
+        print('Published messages till now: ', published_messages)
+        current_frame += 1
+        current_frame = current_frame + velocity
+        # count += 1
+        rospy.sleep(0.1) #should be 0.05 as waiting time = 1/20fps
+        break
+
+
+def mainf():
+    print("Waiting for velocity...\n")
+    rospy.init_node('emulator', anonymous=True)
+    rospy.Subscriber("robot_velocity", Int32, velcallback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        mainf()
+    except rospy.ROSInterruptException:
+        pass
