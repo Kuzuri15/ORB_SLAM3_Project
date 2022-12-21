0d52806d7b3ceabe7bfafb5746081b079d11c99b
Vivek_W <vivekw964@gmail.com>
2022-12-20 19:47:38 -0500

    Updated buffer.py
    Added few conditions in callback functions for flag and image data.

--- flex_orbslam/scripts/buffer.py
+++ flex_orbslam/scripts/buffer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import rospy
 import rosbag
+import time
 from std_msgs.msg import String
 from std_msgs.msg import Int32
 from std_msgs.msg import Bool
@@ -8,50 +9,73 @@ from sensor_msgs.msg import Image
 
 vel_pub = rospy.Publisher("robot_velocity", Int32, queue_size=1)
 img_pub = rospy.Publisher('/camera/image_raw', Image, queue_size = 1)
-
+count = 0
+count_2 = 0
+count_img = 0
+velocity = 0
+image_list2 = []
 
 def imagecallback(img):
-    # print("i am inside img callback\n")
-    global img_pub
-    img_msg = img
-    rate = rospy.Rate(2)
-    img_pub.publish(img_msg)
-    print("image published")
-    rospy.sleep(0.1)
+    global img_pub, count_img, count, velocity
+    start_time = time.time()
+
+    # img_msg = img
+    image_list2.append(img)
+    img_msg = image_list2.pop()
+    if image_list2 or img_msg == img:
+        if velocity == 0:
+            rate = rospy.Rate(20)
+            img_pub.publish(img_msg)
+            rate.sleep()
+        elif velocity != 0:
+            rate = rospy.Rate(10)
+            img_pub.publish(img_msg)
+            rate.sleep()
+
+    count_img += 1
+    print("Images published from buffer: ", count_img)
+    print 'Processsing time per loop:', time.time() - start_time
+
 
 
 def tlosscallback(flag):
-    global vel_pub
+    global vel_pub, count, count_2, velocity
     t_flag = flag.data
-    rate = rospy.Rate(2)
-    print("flag value in buffer:", t_flag)
+    rate = rospy.Rate(10)
+    print("Flag value in buffer:", t_flag)
 
-    if t_flag == False:
+    if t_flag == False:  #tracking is going good
+        count = 0
         velocity = 3
         vel_pub.publish(velocity)
-        rospy.sleep(0.1)
-    elif t_flag == True:
-        velocity = 0
-        vel_pub.publish(velocity)
-        rospy.sleep(0.1)
+        rate.sleep()
+            
+    elif t_flag == True:  #if tracking is lost
+        count += 1
+        if count < 20:
+            velocity = 1
+            vel_pub.publish(velocity)
+            rate.sleep()
+        elif count >=20:  #failing to track for long time or local mapping mode is activated
+            velocity = 0
+            vel_pub.publish(velocity)
+            rate.sleep()
 
 
 def main_func():
     print("Starting...")
     global vel_pub
     rospy.init_node('buffer', anonymous=True)
-    rospy.Subscriber("/camera/images", Image, imagecallback)
+    rospy.Subscriber("/camera/images", Image, imagecallback, queue_size = 15)
     rospy.Subscriber("/track_loss", Bool, tlosscallback)
-    # rospy.sleep(0.1)
-    velo = 3
-    count = 0
+    velo = 1
+    ct = 0
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        # print("reached in while")
-        if count <= 3:
+        if ct <= 9:
             # print(count)
             vel_pub.publish(velo)
-            count += 1
-            rospy.sleep(0.05)
+            ct += 1
 
 
 if __name__ == '__main__':
